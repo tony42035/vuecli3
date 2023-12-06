08654f77a1cac22b8468d8af512c2e7940c514c5
Yu-hsun Chuang <tony42035@gmail.com>
2023-12-06 21:37:52 +0800

    test

---
+++ dist/js/about.6040a470.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuecli3"]=self["webpackChunkvuecli3"]||[]).push([[443],{833:function(n,u,e){e.r(u),e.d(u,{default:function(){return f}});var t=e(3396);const a={class:"about"},c=(0,t._)("h1",null,"This is an about page",-1),r=[c];function s(n,u){return(0,t.wg)(),(0,t.iD)("div",a,r)}var i=e(89);const l={},o=(0,i.Z)(l,[["render",s]]);var f=o}}]);
+//# sourceMappingURL=about.6040a470.js.map
